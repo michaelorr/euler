2fe5144c83c5ffd44adbd239af05649605686e58
Michael Orr <michael.orr@foxnews.com>
2013-03-29 16:39:10 -0400

    tweaks for comparing highcards

--- 054.py
+++ 054.py
@@ -32,73 +32,85 @@ class Hand(object):
     self.cards = cards
     self.rank = self.rank()
     self.highcard = self.highcard()
+    self.rank_val = 0
 
   def __gt__(self, hand):
     if self.rank > hand.rank: return True
-    elif self.rank == hand.rank: return self.highcard > hand.highcard
+    elif self.rank == hand.rank: 
+      if (self.rank_val > hand.rank_val): return True
+      else: return self.highcard > hand.highcard
     else: return False
 
   def rank(self):
-    if (self.r_flush()): return ranks['r_flush']
-    if (self.s_flush()): return ranks['s_flush']
-    if (self.quads()): return ranks['quads']
-    if (self.full_house()): return ranks['full_house']
-    if (self.flush()): return ranks['flush']
-    if (self.straight()): return ranks['straight']
-    if (self.trips()): return ranks['trips']
-    if (self.two_pair()): return ranks['two_pair']
-    if (self.pair()): return ranks['pair']
+    if (self.r_flush(True)):
+      return ranks['r_flush']
+    if (self.s_flush(True)):
+      return ranks['s_flush']
+    if (self.quads(True)):
+      return ranks['quads']
+    if (self.full_house(True)):
+      return ranks['full_house']
+    if (self.flush(True)):
+      return ranks['flush']
+    if (self.straight(True)):
+      return ranks['straight']
+    if (self.trips(True)):
+      return ranks['trips']
+    if (self.two_pair(True)):
+      return ranks['two_pair']
+    if (self.pair(True)):
+      return ranks['pair']
     return ranks['high_card']
 
 
   def highcard(self):
     return self.nums()[-1] 
 
-  def two_pair(self):
+  def two_pair(self, set_attr = False):
     found_pair = False
     totals = Counter(self.nums())
     for num in totals:
       if totals[num] == 2 and found_pair: return True
       else: found_pair = True
-    return False    
+    return False
 
-  def pair(self):
+  def pair(self, set_attr = False):
     totals = Counter(self.nums())
     for num in totals:
       if totals[num] == 2: return True
     return False    
 
-  def s_flush(self):
+  def s_flush(self, set_attr = False):
     if (self.flush() and self.straight()): return True
     return False
 
-  def r_flush(self):
+  def r_flush(self, set_attr = False):
     if (self.s_flush() and self.highcard == 'A'): return True
     return False
 
-  def trips(self):
+  def trips(self, set_attr = False):
     totals = Counter(self.nums())
     for num in totals:
       if totals[num] == 3: return True
     return False
 
-  def full_house(self):
+  def full_house(self, set_attr = False):
     if (self.trips() and self.pair()): return True
     return False
 
-  def quads(self):
+  def quads(self, set_attr = False):
     totals = Counter(self.nums())
     for num in totals:
       if totals[num] == 4: return True
     return False
 
-  def flush(self):
+  def flush(self, set_attr = False):
     base_suit = self.cards[0][1]
     for card in self.cards:
       if card[1] != base_suit: return False
     return True
   
-  def straight(self):
+  def straight(self, set_attr = False):
     try:
       for index, num in enumerate(self.nums()):
         if num + 1 != self.nums()[index + 1]:
