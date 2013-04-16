6bf54dd6a8b0dbac02acfd9cbf0dda952e614e19
Michael Orr <michael@orr.co>
2013-04-15 21:48:49 -0400

    renaming for clarity

--- 014.py
+++ 014.py
@@ -1,15 +1,15 @@
 
-def is_odd(n):
+def if_odd(n):
   return 3 * n + 1
 
-def is_even(n):
+def if_even(n):
   return n / 2
 
 def calculate_collatz(n):
   # print "calculating the collatz of %d" % n
   collatz = [n]
   if n != 1:
-    next = is_even(n) if n % 2 == 0 else is_odd(n)
+    next = if_even(n) if n % 2 == 0 else if_odd(n)
     collatz.extend(calculate_collatz(next))
   return collatz
 
